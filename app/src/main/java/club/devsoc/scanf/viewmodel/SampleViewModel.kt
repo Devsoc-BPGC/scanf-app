8cb2ac3c25cff0f88144bcafaf26504a97b5e990
devansh-299 <f20180131@goa.bits-pilani.ac.in>
2020-07-15 22:42:29 +0530

    initial setup - dependencies

--- app/src/main/java/club/devsoc/scanf/viewmodel/SampleViewModel.kt
+++ app/src/main/java/club/devsoc/scanf/viewmodel/SampleViewModel.kt
@@ -1,5 +1,8 @@
 package club.devsoc.scanf.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
 // This file can be modified/updated in the future
-class SampleViewModel {
+class SampleViewModel(application: Application): AndroidViewModel(application) {
 }
