05121bc2276f04829dbec823e3d1ae827838ce11
Adithya2907 <Adithya>
2020-07-30 23:59:11 +0530

    1) Added code to the MainActivity.kt to react to clicking on FAB, which opens the bottom sheet. 2) Clicking on any of the images in the bottom sheet creates a toast. 3) DATA BINDING HAS BEEN USED FOR BOTTOM SHEET, as it crashes the app as it is not part of a coordinator layout anymore. 4) VIEW MODEL HAS NOT BEEN CREATED, as all code is related to on clicks, and involves some sort of a view, hence not included in the view model. 5) Added two images, one for QR code, and another for Document Scanner. 6) Added the FAB included the bottom sheet in activity_main.xml. 7) Created bottom_sheet.xml which has two images. 8) Added data binding to the gradle file.

--- app/src/main/java/club/devsoc/scanf/view/activity/MainActivity.kt
+++ app/src/main/java/club/devsoc/scanf/view/activity/MainActivity.kt
@@ -2,11 +2,81 @@ package club.devsoc.scanf.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.databinding.DataBindingUtil
 import club.devsoc.scanf.R
+import club.devsoc.scanf.databinding.ActivityMainBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var mainBinding: ActivityMainBinding
+    private lateinit var documentScanButton : ImageView
+    private lateinit var qrScanButton : ImageView
+    private lateinit var bottomSheet : ConstraintLayout
+    private lateinit var bottomSheetBehavior : BottomSheetBehavior<ConstraintLayout>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        defineLayouts()
+        initActivity()
+        attachOnClickListeners()
+    }
+
+    private fun defineLayouts(){
+        //defining the main activity data binding
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        //defining the bottom sheet layout
+        bottomSheet  = findViewById(R.id.bottom_sheet_layout)
+    }
+
+    private fun initActivity(){
+        //defining the bottom sheet behaviour
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+
+        //defining the buttons inside the bottom sheet
+        //DATA BINDING IS NOT USED HERE, AS THE LAYOUT NEEDS TO BE A COORDINATOR LAYOUT
+        bottomSheet.apply {
+            documentScanButton = findViewById(R.id.document_scan_btn)
+            qrScanButton = findViewById(R.id.qr_scan_btn)
+        }
+
+        //setting initial state of bottom sheet to hidden
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+    }
+
+    //attaching on click listeners
+    private fun attachOnClickListeners(){
+        documentScanButton.setOnClickListener(this)
+        qrScanButton.setOnClickListener(this)
+        mainBinding.floatingActionButton.setOnClickListener(this)
+        mainBinding.mainActivityLayout.setOnClickListener(this)
+    }
+
+    //toggling bottom sheet visibility when fab is pressed
+    private fun onFabClicked(){
+        if (bottomSheetBehavior.state == 2 || bottomSheetBehavior.state == 5 )
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        else
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+    }
+
+    //whenever the user clicks anywhere on the screen, the bottom sheet is hidden
+    private fun onScreenClicked(){
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.document_scan_btn -> Toast.makeText(this, "Scan document", Toast.LENGTH_SHORT).show()
+            R.id.qr_scan_btn -> Toast.makeText(this, "QR Scanner", Toast.LENGTH_SHORT).show()
+            R.id.floating_action_button ->onFabClicked()
+            R.id.main_activity_layout -> onScreenClicked()
+        }
     }
 }
