c431df2f9aedd0eec7166db13f296a0ee9aee1cd
ayush-sharma2601 <ayushsh2601@gmail.com>
2020-07-31 19:31:03 +0530

    Implemented the QR Code scan functionality. used https://github.com/yuriy-budiyev/code-scanner, an open source repo based on ZXing.
    - created Activity for scanning purpose
    - layout file contains codeScannerView
    - in Kotlin file, used Easy permission to get camera permissions on creation of activity
    - then qr code is scanned automatically and its text is returned in call back
    
    -- added required dependencies(EasyPermissions & CodeScanner) and permissions(Camera)

---
+++ app/src/main/java/club/devsoc/scanf/view/activity/QRScanActivity.kt
@@ -0,0 +1,75 @@
+package club.devsoc.scanf.view.activity
+
+import android.Manifest
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import club.devsoc.scanf.R
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
+import kotlinx.android.synthetic.main.activity_q_r_scan.*
+import pub.devrel.easypermissions.EasyPermissions
+
+class QRScanActivity : AppCompatActivity() {
+
+    private lateinit var codeScanner: CodeScanner;
+    private val CAMERA_PERMISSION_REQUEST_CODE = 778;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_q_r_scan)
+
+        if (EasyPermissions.hasPermissions(this, Manifest.permission.CAMERA)) {
+            codeScanner.startPreview();
+        } else {
+            EasyPermissions.requestPermissions(this,
+                "Would you like to give this app the permissions to access Camera?",
+                CAMERA_PERMISSION_REQUEST_CODE, Manifest.permission.CAMERA);
+        }
+
+        // Parameters (default values) can be changed according to needs
+        codeScanner.camera = CodeScanner.CAMERA_BACK // or CAMERA_FRONT or specific camera id
+        codeScanner.formats = CodeScanner.ALL_FORMATS // list of type BarcodeFormat,
+        // ex. listOf(BarcodeFormat.QR_CODE)
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE // or CONTINUOUS
+        codeScanner.scanMode = ScanMode.SINGLE // or CONTINUOUS or PREVIEW
+        codeScanner.isAutoFocusEnabled = true // Whether to enable auto focus or not
+        codeScanner.isFlashEnabled = false // Whether to enable flash or not
+
+        // Callbacks for qr scanner
+        codeScanner.decodeCallback = DecodeCallback {
+            runOnUiThread {
+                //scanned info is stored in default variable 'it'.
+            }
+        }
+        codeScanner.errorCallback = ErrorCallback { // or ErrorCallback.SUPPRESS
+            runOnUiThread {
+//                error message stored in it.message
+            }
+        }
+
+        this.scanner_view.setOnClickListener{
+            codeScanner.startPreview();
+        }
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+}
