51ac8ebe6a13c51ed0ba39e2de2da99ddca806b9
devansh-299 <f20180131@goa.bits-pilani.ac.in>
2020-08-02 22:36:19 +0530

    fixed build failures

--- app/src/main/java/club/devsoc/scanf/view/activity/MainActivity.kt
+++ app/src/main/java/club/devsoc/scanf/view/activity/MainActivity.kt
@@ -13,22 +13,21 @@ import com.google.android.material.bottomsheet.BottomSheetBehavior
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
-import club.devsoc.scanf.R
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var mainBinding: ActivityMainBinding
-    private lateinit var documentScanButton : ImageView
-    private lateinit var qrScanButton : ImageView
-    private lateinit var bottomSheet : ConstraintLayout
-    private lateinit var bottomSheetBehavior : BottomSheetBehavior<ConstraintLayout>
-    var drawerLayout: DrawerLayout? = null
+    private lateinit var documentScanButton: ImageView
+    private lateinit var qrScanButton: ImageView
+    private lateinit var bottomSheet: ConstraintLayout
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+    private var drawerLayout: DrawerLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-      
-      drawerLayout = findViewById(R.id.drawer_layout)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
         val navigationView = findViewById<NavigationView>(R.id.nav_view)
         navigationView.setNavigationItemSelectedListener(this)
         val toggle = ActionBarDrawerToggle(
@@ -47,7 +46,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         initActivity()
         attachOnClickListeners()
     }
-  override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
         when (menuItem.itemId) {
             R.id.nav_settings -> {
             }
@@ -57,56 +56,63 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
         }
         return true
-  }
-    private fun defineLayouts(){
-        //defining the main activity data binding
-        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        //defining the bottom sheet layout
-        bottomSheet  = findViewById(R.id.bottom_sheet_layout)
     }
 
-    private fun initActivity(){
-        //defining the bottom sheet behaviour
+    private fun defineLayouts() {
+        // defining the main activity data binding
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // defining the bottom sheet layout
+        bottomSheet = findViewById(R.id.bottom_sheet_layout)
+    }
+
+    private fun initActivity() {
+        // defining the bottom sheet behaviour
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
 
-        //defining the buttons inside the bottom sheet
-        //DATA BINDING IS NOT USED HERE, AS THE LAYOUT NEEDS TO BE A COORDINATOR LAYOUT
+        // defining the buttons inside the bottom sheet
+        // DATA BINDING IS NOT USED HERE, AS THE LAYOUT NEEDS TO BE A COORDINATOR LAYOUT
         bottomSheet.apply {
             documentScanButton = findViewById(R.id.document_scan_btn)
             qrScanButton = findViewById(R.id.qr_scan_btn)
         }
 
-        //setting initial state of bottom sheet to hidden
+        // setting initial state of bottom sheet to hidden
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
     }
 
-    //attaching on click listeners
-    private fun attachOnClickListeners(){
+    // attaching on click listeners
+    private fun attachOnClickListeners() {
         documentScanButton.setOnClickListener(this)
         qrScanButton.setOnClickListener(this)
         mainBinding.floatingActionButton.setOnClickListener(this)
         mainBinding.mainActivityLayout.setOnClickListener(this)
     }
 
-    //toggling bottom sheet visibility when fab is pressed
-    private fun onFabClicked(){
-        if (bottomSheetBehavior.state == 2 || bottomSheetBehavior.state == 5 )
+    // toggling bottom sheet visibility when fab is pressed
+    private fun onFabClicked() {
+        if (bottomSheetBehavior.state == 2 || bottomSheetBehavior.state == 5)
             bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
         else
             bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
     }
 
-    //whenever the user clicks anywhere on the screen, the bottom sheet is hidden
-    private fun onScreenClicked(){
+    // whenever the user clicks anywhere on the screen, the bottom sheet is hidden
+    private fun onScreenClicked() {
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
     }
 
     override fun onClick(v: View?) {
-        when(v?.id){
-            R.id.document_scan_btn -> Toast.makeText(this, "Scan document", Toast.LENGTH_SHORT).show()
-            R.id.qr_scan_btn -> Toast.makeText(this, "QR Scanner", Toast.LENGTH_SHORT).show()
-            R.id.floating_action_button ->onFabClicked()
+        when (v?.id) {
+            R.id.document_scan_btn -> Toast.makeText(
+                this,
+                getString(R.string.scan_document),
+                Toast.LENGTH_SHORT).show()
+            R.id.qr_scan_btn -> Toast.makeText(
+                this,
+                getString(R.string.qr_scanner),
+                Toast.LENGTH_SHORT).show()
+            R.id.floating_action_button -> onFabClicked()
             R.id.main_activity_layout -> onScreenClicked()
         }
     }
