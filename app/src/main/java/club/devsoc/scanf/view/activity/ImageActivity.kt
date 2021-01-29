02f93b4452fbe58cb006dff34fac6cc0d5679374
Rishabh Singh <rishabh.1056@gmail.com>
2021-01-30 00:15:54 +0530

    1- ImageActivity.kt has been appended 2- Layout file activity_image.xml has been appended

--- app/src/main/java/club/devsoc/scanf/view/activity/ImageActivity.kt
+++ app/src/main/java/club/devsoc/scanf/view/activity/ImageActivity.kt
@@ -2,11 +2,27 @@ package club.devsoc.scanf.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import club.devsoc.scanf.R
 
 class ImageActivity : AppCompatActivity() {
+
+    private lateinit var addImageBtn:ImageView
+    private lateinit var okBtn:ImageView
+    private lateinit var imageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image)
+
+        initActivity()
+
+    }
+
+    private fun initActivity()
+    {
+        okBtn=findViewById(R.id.image_activity_okbtn)
+        addImageBtn=findViewById(R.id.image_activity_addimg)
+        imageView=findViewById(R.id.image_activity_imgvw)
     }
 }
