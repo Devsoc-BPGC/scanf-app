aca5167e74bd8f41cb38536fce6b14a4b59ac9eb
ayush-sharma2601 <ayushsh2601@gmail.com>
2021-01-31 00:05:46 +0530

    6th Commit to QR Scan Activity. Changes: - camera screen added

--- app/src/main/java/club/devsoc/scanf/view/activity/ImageActivity.kt
+++ app/src/main/java/club/devsoc/scanf/view/activity/ImageActivity.kt
@@ -1,7 +1,10 @@
 package club.devsoc.scanf.view.activity
 
+import android.Manifest
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,9 +12,14 @@ import android.os.Environment
 import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageView
+import androidx.core.app.ActivityCompat
 import androidx.core.content.FileProvider
 import club.devsoc.scanf.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.priyankvasa.android.cameraviewex.CameraView
+import com.priyankvasa.android.cameraviewex.ErrorLevel
+import com.priyankvasa.android.cameraviewex.Image
+import com.priyankvasa.android.cameraviewex.Modes
 import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
@@ -26,16 +34,47 @@ class ImageActivity : AppCompatActivity() {
     val REQUEST_IMAGE_CAPTURE = 1
     lateinit var currentPhotoPath: String
 
+    private lateinit var camera:CameraView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image)
 
-
-
-
         initActivity()
 
-//        addImageBtn.setOnClickListener(View.OnClickListener { dispatchTakePictureIntent() })
+        camera.addCameraOpenedListener { /* Camera opened. */ }
+            .addCameraErrorListener { t: Throwable, errorLevel: ErrorLevel -> /* Camera error! */ }
+            .addCameraClosedListener { /* Camera closed. */ }
+
+        // enable only single capture mode
+        camera.setCameraMode(Modes.CameraMode.SINGLE_CAPTURE)
+
+        // OR keep other modes as is and enable single capture mode
+        camera.enableCameraMode(Modes.CameraMode.SINGLE_CAPTURE)
+
+        // Output format is whatever set for [app:outputFormat] parameter
+        // Callback on UI thread
+        camera.addPictureTakenListener { image: Image -> /* Picture taken. */ }
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        camera.capture()
+
+        // Disable single capture mode
+        camera.disableCameraMode(Modes.CameraMode.SINGLE_CAPTURE)
+
+        //addImageBtn.setOnClickListener(View.OnClickListener { dispatchTakePictureIntent() })
 
 
         /*on clicking photobutton
@@ -44,6 +83,35 @@ class ImageActivity : AppCompatActivity() {
 
 
     }
+
+    override fun onResume() {
+        super.onResume()
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        camera.start()
+    }
+
+    override fun onPause() {
+        camera.stop()
+        super.onPause()
+    }
+
+//    override fun onDestroyView() {
+//        camera.destroy()
+//        super.onDestroyView()
+//    }
 
 //    private fun dispatchTakePictureIntent() {
 //        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
@@ -88,10 +156,13 @@ class ImageActivity : AppCompatActivity() {
 //    }
 
 
+
+
     private fun initActivity()
     {
         okBtn=findViewById(R.id.image_activity_okbtn)
         addImageBtn=findViewById(R.id.image_activity_addimg)
-        imageView=findViewById(R.id.image_activity_imgvw)
+//        imageView=findViewById(R.id.image_activity_imgvw)
+        camera = findViewById(R.id.camera)
     }
 }
