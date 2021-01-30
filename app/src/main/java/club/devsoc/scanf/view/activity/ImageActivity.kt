a46f29853dc6b9fcb57363a12781824fa6105e45
Rishabh Singh <rishabh.1056@gmail.com>
2021-01-30 22:09:24 +0530

    1- functions for image capturing and storing have been created. 2- manifest file has been appended 3- new xml directory has been created in the res folder

--- app/src/main/java/club/devsoc/scanf/view/activity/ImageActivity.kt
+++ app/src/main/java/club/devsoc/scanf/view/activity/ImageActivity.kt
@@ -1,11 +1,21 @@
 package club.devsoc.scanf.view.activity
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.widget.ImageView
+import androidx.core.content.FileProvider
 import club.devsoc.scanf.R
 import com.camerakit.CameraKitView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ImageActivity : AppCompatActivity() {
 
@@ -14,6 +24,8 @@ class ImageActivity : AppCompatActivity() {
     private lateinit var addImageBtn:ImageView
     private lateinit var okBtn:ImageView
     private lateinit var imageView: ImageView
+    val REQUEST_IMAGE_CAPTURE = 1
+    lateinit var currentPhotoPath: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +39,54 @@ class ImageActivity : AppCompatActivity() {
 
         initActivity()
 
+        /*on clicking photobutton
+        dispatchTakePictureIntent()
+        */
+
     }
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.example.android.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+        }
+    }
+
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File = getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
 
     private fun initActivity()
     {
