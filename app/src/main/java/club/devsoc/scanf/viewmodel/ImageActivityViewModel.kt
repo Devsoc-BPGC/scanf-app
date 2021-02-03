2093319cd7e6887f1e57d49bb8a2ad69911e676d
Rishabh Singh <rishabh.1056@gmail.com>
2021-02-03 23:15:11 +0530

    1- getOutputFile function has been created

--- app/src/main/java/club/devsoc/scanf/viewmodel/ImageActivityViewModel.kt
+++ app/src/main/java/club/devsoc/scanf/viewmodel/ImageActivityViewModel.kt
@@ -1,8 +1,19 @@
 package club.devsoc.scanf.viewmodel
 
+import android.net.Uri
 import androidx.lifecycle.ViewModel
+import java.io.File
 
 class ImageActivityViewModel:ViewModel() {
 
+    public fun createPdf(uri: ArrayList<Uri>)
+    {
+
+    }
+
+    public fun getOutputFile()
+    {
+        //val root: File = File(this.getExternalFilesDir(null), "My PDF Folder")
+    }
 
 }
