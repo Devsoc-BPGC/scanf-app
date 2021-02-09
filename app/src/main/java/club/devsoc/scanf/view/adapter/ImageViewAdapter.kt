74c77d29422437648e21253e5dbfaaa1d7d210b2
ayush-sharma2601 <ayushsh2601@gmail.com>
2021-02-09 20:29:18 +0530

    Added adapter

---
+++ app/src/main/java/club/devsoc/scanf/view/adapter/ImageViewAdapter.kt
@@ -0,0 +1,43 @@
+package club.devsoc.scanf.view.adapter
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import club.devsoc.scanf.R
+import club.devsoc.scanf.model.ImageModel
+
+class ImageViewAdapter(private var images: ArrayList<ImageModel> ):
+    RecyclerView.Adapter<ImageViewAdapter.ImageVH>() {
+    class ImageVH(itemView: View):RecyclerView.ViewHolder(itemView) {
+        fun bind(image:ImageModel){
+//            itemView.image = image.bitmap
+
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)=
+        ImageVH(LayoutInflater.from(parent.context).inflate(R.layout.scanned_image_view,parent,false))
+
+    override fun onBindViewHolder(holder: ImageViewAdapter.ImageVH, position: Int) {
+        holder.bind(images[position])
+    }
+
+    override fun getItemCount(): Int {
+        return images.size
+    }
+
+    fun addData(list: ArrayList<ImageModel>) {
+//        users.clear()
+//        users = list;
+        images.addAll(list)
+    }
+
+    fun clearList() {
+        images.clear()
+//        Log.d("TAG", "clearList: >>>>>>>>>>>>>>>>>>>>>>>>>>> ${images.size}")
+
+    }
+}
