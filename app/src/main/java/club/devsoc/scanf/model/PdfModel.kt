1cc9c66e4ef3817a4d48bbc606631fb4cee222b1
Rishabh Singh <rishabh.1056@gmail.com>
2021-04-08 19:31:01 +0530

    1- Adapter has been made 2- Model class has been made 3- Itemview xml has been made

---
+++ app/src/main/java/club/devsoc/scanf/model/PdfModel.kt
@@ -0,0 +1,8 @@
+package club.devsoc.scanf.model
+
+import android.graphics.Bitmap
+
+data class PdfModel(var bitmap : Bitmap,var name : String)
+{
+
+}
---
+++ app/src/main/java/club/devsoc/scanf/view/adapter/PdfRvAdapter.kt
@@ -0,0 +1,39 @@
+package club.devsoc.scanf.view.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import club.devsoc.scanf.R
+import club.devsoc.scanf.model.PdfModel
+import kotlinx.android.synthetic.main.pdf_files_view.view.*
+
+class PdfRvAdapter(private var pdfModel : ArrayList<PdfModel>):RecyclerView.Adapter<PdfRvAdapter.PdfVH>() {
+
+    class PdfVH(itemView: View) :RecyclerView.ViewHolder(itemView){
+
+        fun bind(pdfModel: PdfModel)
+        {
+            itemView.pdf_files_imgvw.setImageBitmap(pdfModel.bitmap)
+            itemView.pdf_files_nametv.setText(pdfModel.name)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)=
+        PdfVH(LayoutInflater.from(parent.context).inflate(R.layout.pdf_files_view,parent,false))
+
+
+    override fun onBindViewHolder(holder: PdfVH, position: Int) {
+        holder.bind(pdfModel[position])
+    }
+
+    override fun getItemCount(): Int {
+        return pdfModel.size
+    }
+
+    fun addData(list: ArrayList<PdfModel>)
+    {
+        pdfModel.addAll(list)
+    }
+}
