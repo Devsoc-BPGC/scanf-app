653bfb268b7802eaee6837887d973f57b2029838
rushabh101 <rushah101@gmail.com>
2022-03-24 06:04:45 +0530

    with extra activity

---
+++ app/src/main/java/club/devsoc/scanf/model/GalleryModel.kt
@@ -0,0 +1,8 @@
+package club.devsoc.scanf.model
+
+import android.net.Uri
+
+data class GalleryModel(var path : Uri)
+{
+
+}
--- app/src/main/java/club/devsoc/scanf/view/activity/HomeActivity.kt
+++ app/src/main/java/club/devsoc/scanf/view/activity/HomeActivity.kt
@@ -2,6 +2,8 @@ package club.devsoc.scanf.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import club.devsoc.scanf.R
 import club.devsoc.scanf.view.fragment.HomeFragment
 
@@ -10,6 +12,17 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity)
+
+        val requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    Log.i("Permission: ", "Granted")
+                } else {
+                    Log.i("Permission: ", "Denied")
+                }
+            }
 
     }
 }
